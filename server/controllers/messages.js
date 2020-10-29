b2d1b7193b8b23a06f7d89a3fb3fba97a88850a6
Sean Piner <sean.piner@gmail.com>
2020-10-28 22:24:15 -0700

    bedtime commit

--- server/controllers/messages.js
+++ server/controllers/messages.js
@@ -1,6 +1,10 @@
 var models = require('../models');
 //TODO
 module.exports = {
-  get: function (req, res) {}, // a function which handles a get request for all messages
-  post: function (req, res) {} // a function which handles posting a message to the database
+  get: function (req, res) {
+
+  }, // a function which handles a get request for all messages
+  post: function (req, res) {
+    // use server-spec 30-66 as template
+  } // a function which handles posting a message to the database
 };
